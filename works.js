e1c50d9d99fe4ed09e703cadb924f6e7f069183c
xxvictoriaestupinanxx-star <xxvictoriaestupinanxx@gmail.com>
2025-12-04 00:56:53 -0500

    Update works.js

--- works.js
+++ works.js
@@ -2,47 +2,45 @@ console.log("Gallery script loaded");
 
 let currentIndex = 0;
 
-// Image list using EXACT filenames you provided
+
 const works = [
-  { image: "images/2d.jpg", description: "2D Artwork" },
-  { image: "images/2dwork1.jpg", description: "2D Work 1" },
-  { image: "images/03bkpp.jpg", description: "03 BKPP" },
-  { image: "images/O3nyc.jpg", description: "O3 NYC" },
-  { image: "images/03nусс.jpg", description: "03 nусс" },
-  { image: "images/20anos.jpg", description: "20 años" },
-  { image: "images/2023.jpg", description: "2023 Work" },
-  { image: "images/bksp.jpg", description: "BKSP" },
-  { image: "images/chikibb.jpg", description: "Chikibb" },
-  { image: "images/cuca.jpg", description: "Cuca" },
-  { image: "images/delnpals.jpg", description: "Deln Pals" },
-  { image: "images/IMG_3410.JPG", description: "IMG 3410" },
-  { image: "images/kelso.jpg", description: "Kelso" },
-  { image: "images/kelsoo.JPEG", description: "Kelsoo" },
-  { image: "images/macho.jpg", description: "Macho" },
-  { image: "images/mami.jpg", description: "Mami" },
-  { image: "images/meowweb.jpg", description: "Meow Web" },
-  { image: "images/new.jpg", description: "New Work" },
-  { image: "images/new2d.jpg", description: "New 2D" },
-  { image: "images/neweeb.jpg", description: "New EEB" },
-  { image: "images/newnew.jpg", description: "New New" },
-  { image: "images/newweb.jpg", description: "New Web" },
-  { image: "images/newwebbb.jpg", description: "New Web BB" },
-  { image: "images/Papi.jpg", description: "Papi" },
-  { image: "images/performance.jpg", description: "Performance" },
-  { image: "images/thatnewneww.jpg", description: "That New New" },
+  { image: "images/2d.jpg", description: "" },
+  { image: "images/2dwork1.jpg", description: "" },
+  { image: "images/03bkpp.jpg", description: "" },
+  { image: "images/O3nyc.jpg", description: "" },
+  { image: "images/03nycc.jpg", description: "" },
+  { image: "images/20anos.jpg", description: "" },
+  { image: "images/2023.jpg", description: "" },
+  { image: "images/bksp.jpg", description: "" },
+  { image: "images/chikibb.jpg", description: "" },
+  { image: "images/cuca.jpg", description: "" },
+  { image: "images/delnpals.jpg", description: "" },
+  { image: "images/kelso.jpg", description: "" },
+  { image: "images/kelsoo.JPEG", description: "" },
+  { image: "images/macho.jpg", description: "" },
+  { image: "images/mami.jpg", description: "" },
+  { image: "images/meowweb.jpg", description: "" },
+  { image: "images/new.jpg", description: "" },
+  { image: "images/new2d.jpg", description: "" },
+  { image: "images/neweeb.jpg", description: "" },
+  { image: "images/newnew.jpg", description: "" },
+  { image: "images/newweb.jpg", description: "" },
+  { image: "images/newwebbb.jpg", description: "" },
+  { image: "images/Papi.jpg", description: "" },
+  { image: "images/performance.jpg", description: "" },
+  { image: "images/thatnewneww.jpg", description: "" },
   { image: "images/thefirstconnection.jpg", description: "The First Connection" },
-  { image: "images/utero.jpg", description: "Utero" },
-  { image: "images/webhomepage.jpg", description: "Web Homepage" },
-  { image: "images/webimagenew.jpg", description: "Web Image New" }
+  { image: "images/utero.jpg", description: "" },
+  { image: "images/webimagenew.jpg", description: "" }
 ];
 
-// DOM elements
+
 const imageElement = document.getElementById("workImage");
 const descriptionElement = document.getElementById("workDescription");
 const prevBtn = document.getElementById("prevBtn");
 const nextBtn = document.getElementById("nextBtn");
 
-// Update slide
+
 function updateGallery(index) {
   const work = works[index];
   imageElement.src = work.image;
@@ -50,10 +48,10 @@ function updateGallery(index) {
   descriptionElement.textContent = work.description;
 }
 
-// Initial load
+
 updateGallery(currentIndex);
 
-// Buttons
+
 prevBtn.addEventListener("click", () => {
   currentIndex = (currentIndex - 1 + works.length) % works.length;
   updateGallery(currentIndex);
